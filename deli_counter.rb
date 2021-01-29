f3a2302846fee6f77d156d41f0daf941bd32b8b0
Lena Hou <lenahou2013@gmail.com>
2021-01-29 19:59:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 katz_deli = []
 
 def line(katz_deli)
-  line = ["The line is currently: "]
+  line = "The line is currently: "
   katz_deli.each_with_index { |name, index|
     list.push("#{index + 1}. #{}{name}")
     }
