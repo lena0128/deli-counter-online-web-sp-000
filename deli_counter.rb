4f9e35524daf63412875c0bc52dd50637cf333a7
Lena Hou <lenahou2013@gmail.com>
2021-01-29 20:12:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 # Write your code here.
-
+katz_deli = []
 
 def line(katz_deli)
 if katz_deli.size == 0
-  return "The line is currently empty."
+  return "The line is empty."
 else katz_deli.size > 0
 line_list = "The line is currently: "
 katz_deli.each_with_index do |name, index|
