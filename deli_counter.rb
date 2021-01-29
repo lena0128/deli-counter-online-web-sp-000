5d68f7c4b7e26abd8222fffc7cac3b706eee3335
Lena Hou <lenahou2013@gmail.com>
2021-01-29 20:14:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,13 @@
 katz_deli = []
 
 def line(katz_deli)
-if katz_deli.size == 0
-  puts "The line is currently empty."
-else katz_deli.size > 0
-line_list = "The line is currently: "
-katz_deli.each_with_index do |name, index|
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else katz_deli.size > 0
+    line_list = "The line is currently: "
+    katz_deli.each_with_index do |name, index|
     line_list.push("#{index + 1}. #{name} ")
   end
-  return line_list
+   line_list
 end
 end
